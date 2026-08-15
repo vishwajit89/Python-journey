48ee34c7e16a79452470e4d84fea7ef2f7f02682
Solu kumar <harmonorelato12@gmail.com>
2026-08-16 00:30:50 +0530

    Create 01-basics/02_operators.py
    learned assignment, comparison, logical operators + type casting with input()

---
+++ 01-basics/02_operators.py
@@ -0,0 +1,30 @@
+# Assignment operators
+a = 4 - 3  # assign 4-3 in a
+print(a)
+
+b = 6
+b += 3  # increment the value by 3
+b -= 2  # decrement the value
+print(b)
+
+# Comparison operator
+d = 5 == 4
+print(d)
+
+# Logical operator
+e = True or False
+print(e)
+
+a = "31.7"
+b = float(a)
+t = type(b)
+print(t)
+
+a = int(input("enter number 1: "))
+b = int(input("enter number 2: "))
+print("number a is:", a)
+print("number b is:", b)
+print("number is", a + b)
+
+name = input("enter your name: ")
+print(f"good morning, {name}")
