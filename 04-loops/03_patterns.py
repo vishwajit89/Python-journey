c3bfed1d1ddb0cf92e3355fe2dc7702dfc7560df
Solu kumar <harmonorelato12@gmail.com>
2026-08-20 22:05:06 +0530

    Create 03_patterns.py
    star patterns: pyramid with spacing, simple left-aligned triangle

---
+++ 04-loops/03_patterns.py
@@ -0,0 +1,25 @@
+'''
+For n = 3
+  *
+ ***
+*****
+
+For n = 5
+    *
+   ***
+  *****
+ ********
+**********
+'''
+
+n = int(input("Enter the number: "))
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    print("*" * (2 * i - 1), end="")
+    print("")
+
+
+n = int(input("Enter the number: "))
+for i in range(1, n + 1):
+    print("*" * i, end="")
+    print("")
