2806c9e75267e42a14426a5713526036967f5eee
Solu kumar <harmonorelato12@gmail.com>
2026-08-21 23:33:05 +0530

    Create 05-functions/01_function_basics.py
    functions: definition/call basics, why functions avoid repetition, params + return values, default parameter values

---
+++ 05-functions/01_function_basics.py
@@ -0,0 +1,39 @@
+# Function Definition
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+    average = (a + b + c) / 3
+    print(average)
+
+avg()  # Function Call
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+avg()
+
+
+def goodDay():
+    print("Good Day")
+
+goodDay()
+
+
+def goodDay(name, ending):
+    print("Good Day, " + name)
+    print(ending)
+    return "ok"
+
+a = goodDay("Harry", "Thank you")
+print(a)
+
+
+def goodDay(name, ending="Thank you"):
+    print(f"Good Day, {name}")
+    print(ending)
+
+goodDay("Harry", "Thanks")
+goodDay("Rohan")
